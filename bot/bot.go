f7a7a757535c2aaf8375facf27c0a3bcb137b84f
anildevdutt <anil.devdutt@gmail.com>
2023-01-11 09:25:08 +0000

    updated

--- bot/bot.go
+++ bot/bot.go
@@ -21,11 +21,11 @@ func chkerr(e error) {
 func SaveUserData(userdata igapi.UserData) {
 	user := userdata.Data.User
 	insert := "INSERT INTO users VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-	_, err := db.Exec(insert, user.Id, user.Username, user.Full_name, user.Biography, user.External_url, 
-		user.Edge_followed_by.Count, user.Edge_follow.Count, user.Fbid, user.Has_clips, user.Has_channel,user.Highlight_reel_count, 
-		user.Hide_like_and_view_counts, user.Is_business_account, user.Is_professional_account,user.Is_supervision_enabled, 
+	_, err := db.Exec(insert, user.Id, user.Username, user.Full_name, user.Biography, user.External_url,
+		user.Edge_followed_by.Count, user.Edge_follow.Count, user.Fbid, user.Has_clips, user.Has_channel, user.Highlight_reel_count,
+		user.Hide_like_and_view_counts, user.Is_business_account, user.Is_professional_account, user.Is_supervision_enabled,
 		user.Is_joined_recently, user.Guardian_id, user.Business_email, user.Business_phone_number, user.Business_contact_method,
-		user.Business_category_name, user.Category_name, user.Is_private, user.Is_verified, user.Profile_pic_url_hd,user.Connected_fb_page)
+		user.Business_category_name, user.Category_name, user.Is_private, user.Is_verified, user.Profile_pic_url_hd, user.Connected_fb_page)
 	chkerr(err)
 }
 
@@ -34,7 +34,7 @@ func Start() {
 	db, err = sql.Open("sqlite3", dbfile)
 	chkerr(err)
 
-	userdata := igapi.GetUserNameInfo("everydayaniime")
+	userdata := igapi.GetUserNameInfo("f1")
 	log.Println(userdata)
 	// SaveUserData(userdata)
 }
