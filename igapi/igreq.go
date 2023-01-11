3082da0ef7841a6feb3a178e5785dddd12b3d7e6
anildevdutt <anil.devdutt@gmail.com>
2023-01-11 09:38:37 +0000

    updated

--- igapi/igreq.go
+++ igapi/igreq.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 )
 
-var useragent = "Instagram 244.0.0.17.110 Android"
+var useragent = "Instagram 219.0.0.12.117 Android"
 
 func GetRequest(requrl string) []byte {
 	client := &http.Client{}
 
 	req, _ := http.NewRequest("GET", requrl, nil)
-	// req.Header.Add("user-agent", useragent)
+	req.Header.Add("user-agent", useragent)
 	// req.Header.Add("cookie", ``)
 
 	resp, err := client.Do(req)
